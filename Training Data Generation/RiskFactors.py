2289a78739cf6bd1aef155993777df417821e500
Eli Winkelman <ewink20@icloud.com>
2024-01-10 10:46:33 +1300

    add comments
    Former-commit-id: ed8cf5748a4fd37c6ae234c25caedead7c4b2879

--- Training Data Generation/RiskFactors.py
+++ Training Data Generation/RiskFactors.py
@@ -4,6 +4,12 @@
 import numpy as np
 import math
 def BOUNDARIES_BASINS(idx):
+    '''
+    Copied from STORM model
+
+    :param idx: basin index
+    :return:
+    '''
     if idx == 'EP':  # Eastern Pacific
         lat0, lat1, lon0, lon1 = 5, 60, 180, 285
     if idx == 'NA':  # North Atlantic
@@ -21,8 +27,8 @@ def BOUNDARIES_BASINS(idx):
 
 def createMonthlyLandfallGrid(basin, resolution):
     '''
-
-    :param basin: which basin is the grid for?
+    Generate a grid to store TC monthly landfall statistics
+    :param basin
     :param resolution: how many degrees each bin should be
     :return:
     '''
@@ -38,10 +44,13 @@ def createMonthlyLandfallGrid(basin, resolution):
     )
 
 def getGridCell(lat, lon, resolution, basin):
-    print(lat, lon, resolution)
+    '''
+    Get the grid cell for given latitude, longitude, and grid resolution
+
+    :return: indices of the lat and lon cells respectively
+    '''
 
     lat0, lat1, lon0, lon1 = BOUNDARIES_BASINS(basin)
-
 
     latCell = math.floor((lat - lat0) * 1/resolution)
     lonCell = math.floor((lon - lon0) * 1/resolution)
@@ -49,6 +58,18 @@ def getGridCell(lat, lon, resolution, basin):
     return latCell, lonCell
 
 def averageLandfallsPerMonth(TC_data, basin, total_years, resolution):
+    """
+    Calculate average landfalls per month within lat, lon bins from synthetic TC data.
+    For use as the training output of ML model.
+
+    :param TC_data: list of synthetically generated TC data
+    :param basin: the basin storms are being generated in
+    :param total_years: number of years that synthetic data was generated over
+    :param resolution: the lat, lon resolution to calculate statistics for in degrees
+    :return: a lat, lon, month matrix for the basin with values
+             the average number of landfalls in that month in the provided TC data
+    """
+
     # TC_data is list of [year, storm_number, genesis_month, lat, lon, landfalllist]
 
     grid = createMonthlyLandfallGrid(basin, resolution)
