567c5164351df76121dda10caf1b9e2d1027f4ce
Eli Winkelman eli.winkelman@auckland.ac.nz <ewin313@mahuika01.mahuika.nesi.org.nz>
2024-02-26 12:44:26 +1300

    Adjust learning rate and training code
    Former-commit-id: 67a44552e83762938c12f6fa5c1db1bc95b61a01

--- MachineLearning/ConvProbPredictor/train.py
+++ MachineLearning/ConvProbPredictor/train.py
@@ -1,6 +1,8 @@
 
-from ..dataset import get_dataset
-from conv_prob_predictor import *
+from MachineLearning.dataset import get_dataset
+from MachineLearning.ConvProbPredictor.conv_prob_predictor import *
+import time
+
 def train(data_folder):
 
     train_data = get_dataset(data_folder, data_version=2)
@@ -20,12 +22,12 @@ def train(data_folder):
 
     # TODO: add CPRS metric
     model.compile(
-        optimizer=optimizers.Adam(learning_rate=0.0005),
+        optimizer=optimizers.Adam(learning_rate=0.0001),
         loss=NegLogLik
     )
 
     model.fit(train_data,
-              epochs=1,
+              epochs=20,
               validation_data=validation_data,
               verbose=2,
               callbacks=[callback]
@@ -38,4 +40,3 @@ def train(data_folder):
 
     model.save_weights("models/site_prob_{}.weights.h5".format(str(time.time())))
     #model.save('models/site_prob_{}.keras'.format(str(time.time())))
-
