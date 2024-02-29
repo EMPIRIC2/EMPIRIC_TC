389e97821610f781125e5c7aa2a80fb9e6bc4fe6
Eli Winkelman <ewink20@icloud.com>
2024-03-01 10:48:20 +1300

    select combinations randomly so they aren't mostly overlapping
    Former-commit-id: e5f4e45d3ecf1d1d112dced3cd3d2afdae678758

--- MachineLearning/dataset.py
+++ MachineLearning/dataset.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import os
 import glob
 import itertools
-
+import random
 months = [1,2,3,4,11,12]
 
 
@@ -45,6 +45,25 @@ class hdf5_generator_v2:
         self.dataset = dataset
         self.year_grouping_size = year_grouping_size
 
+    def _get_random_year_combination(self, num_years, size):
+        sample = set()
+        while len(sample) < size:
+            # Choose one random item from items
+            elem = random.randint(0, num_years-1)
+            # Using a set elminates duplicates easily
+            sample.add(elem)
+        return tuple(sample)
+
+
+    def get_random_year_combinations(self, num_combinations, num_years, size):
+
+        samples = set()
+        while len(samples) < num_combinations:
+            comb = self._get_random_year_combination(num_years, size)
+            samples.add(comb)
+
+        return tuple(samples)
+
     def __call__(self):
 
         for file_path in self.file_paths:
@@ -53,20 +72,26 @@ class hdf5_generator_v2:
                 movements = file[self.dataset + "_movement"]
 
                 outputs = file[self.dataset + "_sites"]
+
+                #randomly select combinations of years for training
+                year_indices = self.get_random_year_combinations(1000, 1000, 10)
+
+                # keep test samples with genesis matrix in order to make it simpler to assess the real output distribution
                 if self.dataset == "test":
                     for genesis, movement, output in zip(geneses, movements, outputs):
                         if np.count_nonzero(genesis) != 0:  # data has been made
-                            for year_indices in itertools.islice(itertools.combinations(list(range(1000)), self.year_grouping_size), 1000):
+                            for year_index in year_indices:
                                 month = 3
-                                yield (np.expand_dims(genesis[month], axis=-1), movement[months[month]]), np.sum(np.sum(output[list(year_indices)], axis=0)[:, month, :], -1)
+                                yield (np.expand_dims(genesis[month], axis=-1), movement[months[month]]), np.sum(np.sum(output[list(year_index)], axis=0)[:, month, :], -1)
                 
                 # spread out training examples with the same genesis matrix to avoid overfitting
-                for year_indices in itertools.islice(itertools.combinations(list(range(1000)), self.year_grouping_size), 1000):
+                for year_index in year_indices:
+
                     for genesis, movement, output in zip(geneses, movements, outputs):
                         if np.count_nonzero(genesis) != 0:  # data has been made
                         # switch the order of genesis matrix and divide output by number of years
                             month = 3
-                            yield (np.expand_dims(genesis[month], axis=-1), movement[months[month]]), np.sum(np.sum(output[list(year_indices)], axis=0)[:, month, :], -1)
+                            yield (np.expand_dims(genesis[month], axis=-1), movement[months[month]]), np.sum(np.sum(output[list(year_index)], axis=0)[:, month, :], -1)
                     else:  # this sample was never generated
                         break
 class hdf5_generator_v1:
@@ -168,6 +193,6 @@ def get_dataset(folder_path, batch_size=32, dataset="train", data_version=0):
 
     return batched_dataset
 
-#test_v3 = get_dataset('../Data/v2/', data_version=3)
+#test_v3 = get_dataset('../Data/v2/', data_version=2)
 #for elem in test_v3.take(1):
     #print(elem)
