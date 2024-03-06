96e88e3d0821974c560a077fe989afc13cbe6c65
Eli Winkelman <ewink20@icloud.com>
2024-03-06 16:34:17 +1300

    generate grids with training data again
    Former-commit-id: e6905507e60f58d43883a25ff27ebb26bbb51f06

--- Training Data Generation/RiskFactors.py
+++ Training Data Generation/RiskFactors.py
@@ -289,17 +289,16 @@ def getLandfallsData(TC_data, basin, total_years, resolution, sites, include_gri
 
         decades_of_storms[decade].append(storm)
 
-
     decade_grids = []
     decade_site_data = []
-
+    print(include_grids)
     with Pool(number_of_cores) as pool:
         args = [(decade,
                  decade_of_storms,
                  resolution,
                  basin,
                  sites,
-                 False
+                 include_grids
                  )
                 for decade, decade_of_storms
                 in enumerate(decades_of_storms)
@@ -309,7 +308,7 @@ def getLandfallsData(TC_data, basin, total_years, resolution, sites, include_gri
 
         for i, decade_result in enumerate(decade_results):
             site_data, decade_grid, decade = decade_result
-
+            print(decade_grid)
             if include_grids:
                 decade_grids.append(decade_grid)
 
