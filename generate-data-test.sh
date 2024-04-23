ec138680995ef7044c641bdcfe61512eff2b4109
Eli Winkelman <ewin313@wbn005.cm.cluster>
2024-04-23 15:06:47 +1200

    adding older changes
    Former-commit-id: 69628e954d11e9fa87f5ad63487e3bd87da7bd2c

--- generate-data-test.sh
+++ generate-data-test.sh
@@ -1,7 +1,7 @@
 python slurm-launch.py \
   --exp-name test_data_generation \
-  --command "python ./Training\ Data\ Generation/GenerateTrainingData.py 100 1 0 0 ./Data/run2" \
+  --command "python ./Training\ Data\ Generation/GenerateTrainingData.py 200 1 0 0 ./Training\ Data\ Generation/data/ --include_grids" \
   --load-env "source load-environment.sh" \
   --mem "300MB" \
-  --time "00:10:00" \
+  --time "00:15:00" \
   --debug True
