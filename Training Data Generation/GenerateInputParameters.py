84b430a085fc14f1b778e7f7e9b3d8add8567431
Eli Winkelman <eliwinkelman2@gmail.com>
2024-06-04 09:43:42 +1200

    update input parameter comments
    Former-commit-id: 2edebfe360ecb43c2a0076077bdd8be9b32314c4

--- Training Data Generation/GenerateInputParameters.py
+++ Training Data Generation/GenerateInputParameters.py
@@ -3,12 +3,9 @@
     used to create a training dataset for machine learning.
 
     For Now we only care about TC genesis and movement, so we need to generate inputs for
-    # storms per year (should be fairly constant)
-    genesis month (also assume constant)
     Genesis location (randomize weighted based on prob. per 1 degree x 1 degree boc)
-    TC track (movement "characteristics for every 5 degree lat. bin and epsilon distribution)
 
-    For now, will randomly perturb parameters from observed data
+
 """
 import unittest
 import numpy as np
