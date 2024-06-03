00d3fbe959c32f0b0a31a3202c6dbf185be6e441
Eli Winkelman <eliwinkelman2@gmail.com>
2024-06-04 10:48:00 +1200

    fix tests
    Former-commit-id: b756fd8935ad5ac631455701c4bd1dc138a4a26b

--- MachineLearning/Evaluation/evaluation_testing_utils.py
+++ MachineLearning/Evaluation/evaluation_testing_utils.py
@@ -1,7 +1,7 @@
 """ Testing Utilities """
 import numpy as np
-import model_statistics
-import metrics
+import MachineLearning.Evaluation.model_statistics as model_statistics
+from MachineLearning.Evaluation.metrics import compute_metrics
 
 def create_test_grid(data):
     test_grid = np.zeros((110, 210))
@@ -11,13 +11,14 @@ def create_test_grid(data):
     return test_grid
 
 test_grid_parameters = [[(55, 45, 10), (100, 50, 15), (55, 34, 5)],
-                        [(55, 45, 5), (100, 50, 20), (55, 34, 2)],
                         [(55, 45, 7), (100, 50, 12), (55, 34, 3)],
                         [(55, 45, 18), (100, 50, 0), (55, 34, 0)],
+                        [(55, 45, 5), (100, 50, 20), (55, 34, 2)],
+                        [(55, 45, 17), (100, 50, 2), (55, 34, 1)],
                         [(55, 45, 11), (100, 50, 12), (55, 34, 6)],
                         [(55, 45, 10), (100, 50, 17), (55, 34, 2)],
-                        [(55, 45, 10), (100, 50, 13), (55, 34, 4)],
-                        [(55, 45, 17), (100, 50, 2), (55, 34, 1)]]
+                        [(55, 45, 10), (100, 50, 13), (55, 34, 4)]
+                        ]
 
 TEST_GRIDS_1 = [create_test_grid(test_grid_parameters[i]) for i in range(4)]
 TEST_GRIDS_2 = [create_test_grid(test_grid_parameters[i]) for i in range(4,8)]
@@ -25,6 +26,6 @@ TEST_GRIDS_2 = [create_test_grid(test_grid_parameters[i]) for i in range(4,8)]
 STATISTICS_1 = model_statistics.compute_ensemble_statistics(TEST_GRIDS_1)
 STATISTICS_2 = model_statistics.compute_ensemble_statistics(TEST_GRIDS_2)
 
-ALL_METRICS = metrics.compute_metrics(TEST_GRIDS_1, TEST_GRIDS_2, STATISTICS_1, STATISTICS_2, "Custom-UNet")
+ALL_METRICS = compute_metrics(TEST_GRIDS_1, TEST_GRIDS_2, STATISTICS_1, STATISTICS_2, "Custom-UNet")
 
 
