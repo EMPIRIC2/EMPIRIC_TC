16f7b1c6a2c299f17dc20d507d06239e6dc95ade
Eli Winkelman <ewink20@icloud.com>
2024-06-05 11:39:52 +1200

    Update MachineLearning/Evaluation/test_evaluation_utils.py
    Co-authored-by: Gideon Dresdner <gideond@gmail.com>
    Former-commit-id: 86bc1847f1fb0e098a897def943930a4da9d0802

--- MachineLearning/Evaluation/test_evaluation_utils.py
+++ MachineLearning/Evaluation/test_evaluation_utils.py
@@ -8,7 +8,7 @@ class TestEvaluationUtils(unittest.TestCase):
     @parameterized.expand([
         (-60, 135, 0.5, (0,0)), (-60, 136.1, 0.5, (0, 2)), (-60, 136.1, 1, (0, 1))
     ])
-    def test_get_grid_cell(self, latitude, longitude, resolution, cell):
+    def test_get_grid_cell(self, latitude, longitude, resolution, expected_cell):
         # test that the get grid cell function works properly for two different resolutions
         self.assertEqual(get_grid_cell(latitude,longitude, resolution), cell)
 
