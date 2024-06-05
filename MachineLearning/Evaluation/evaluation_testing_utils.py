3c7d18da53ff39f114873387cefe30e4ecca0200
Eli Winkelman <ewink20@icloud.com>
2024-06-05 14:19:58 +1200

    flip grid up/down
    Former-commit-id: 5b96f9c04bb6d3469c8729990f5c38bb1c3bb073

--- MachineLearning/Evaluation/evaluation_testing_utils.py
+++ MachineLearning/Evaluation/evaluation_testing_utils.py
@@ -8,14 +8,16 @@ def create_test_grid(data):
 
     return test_grid
 
-test_grid_parameters = [[(55, 45, 10), (100, 50, 15), (55, 34, 5)],
+test_grid_parameters_1 = [[(55, 45, 10), (100, 50, 15), (55, 34, 5)],
                         [(55, 45, 5), (100, 50, 20), (55, 34, 2)],
                         [(55, 45, 7), (100, 50, 12), (55, 34, 3)],
-                        [(55, 45, 18), (100, 50, 0), (55, 34, 0)],
-                        [(55, 45, 11), (100, 50, 12), (55, 34, 6)],
+                        [(55, 45, 18), (100, 50, 0), (55, 34, 0)]
+                        ]
+
+test_grid_parameters_2 = [[(55, 45, 11), (100, 50, 12), (55, 34, 6)],
                         [(55, 45, 10), (100, 50, 17), (55, 34, 2)],
                         [(55, 45, 10), (100, 50, 13), (55, 34, 4)],
                         [(55, 45, 17), (100, 50, 2), (55, 34, 1)]]
 
-TEST_GRIDS_1 = [create_test_grid(test_grid_parameters[i]) for i in range(4)]
-TEST_GRIDS_2 = [create_test_grid(test_grid_parameters[i]) for i in range(4,8)]
+TEST_GRIDS_1 = [create_test_grid(params) for params in test_grid_parameters_1]
+TEST_GRIDS_2 = [create_test_grid(params) for params in test_grid_parameters_2]
--- MachineLearning/Evaluation/test_evaluation_utils.py
+++ MachineLearning/Evaluation/test_evaluation_utils.py
@@ -6,11 +6,11 @@ from parameterized import parameterized
 class TestEvaluationUtils(unittest.TestCase):
 
     @parameterized.expand([
-        (-60, 135, 0.5, (0,0)), (-60, 136.1, 0.5, (0, 2)), (-60, 136.1, 1, (0, 1))
+        (-5.1, 135, 0.5, (0,0)), (-5.1, 136.1, 0.5, (0, 2)), (-5.1, 136.1, 1, (0, 1))
     ])
     def test_get_grid_cell(self, latitude, longitude, resolution, expected_cell):
         # test that the get grid cell function works properly for two different resolutions
-        self.assertEqual(get_grid_cell(latitude,longitude, resolution), cell)
+        self.assertEqual(get_grid_cell(latitude,longitude, resolution), expected_cell)
 
     def test_get_grid_cell_out_of_basin(self):
 
