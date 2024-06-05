db092bc15f9d3948d3dcaa09b8c4aea90c379453
Eli Winkelman <ewink20@icloud.com>
2024-06-06 10:57:35 +1200

    Delete predict_site_probs.py
    Former-commit-id: 481569fd4603a8b5e66bc384619663122f6b24cd

--- predict_site_probs.py
+++
@@ -1,5 +0,0 @@
-from MachineLearning.ConvProbPredictor.predict import make_site_predictions
-print("making site preds")
-make_site_predictions("/nesi/project/uoa03669/ewin313/storm_data/v2/", "/nesi/project/uoa03669/ewin313/TropicalCycloneAI/models/site_prob_discrete_1712630188.1991909.weights.h5", "/nesi/project/uoa03669/ewin313/TropicalCycloneAI/MachineLearning/ConvProbPredictor/predictions", 9)
-
-
