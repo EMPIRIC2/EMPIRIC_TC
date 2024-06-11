8028d442ad9a3f703bec5046f20bd5f0d53f03e4
Eli Winkelman <ewin313@wbn003.cm.cluster>
2024-06-12 09:27:27 +1200

    add song unet
    Former-commit-id: 669c69b230089798a0b909a265d292fe6f7b0a18

--- MachineLearning/Evaluation/model_info.py
+++ MachineLearning/Evaluation/model_info.py
@@ -1,9 +1,14 @@
-from saved_models.saved_models import DDPMUNet02CatCyclones, DDPMUNetNoAttention02CatCyclones, UNetCustom02CatCyclones
+from saved_models.saved_models import DDPMUNet02CatCyclones, DDPMUNetNoAttention02CatCyclones, UNetCustom02CatCyclones, SongUNet
 
 """
 Object keeps track of the models we want to evaluate.
 """
 models_info = [
+    {
+        "Name": "DDIM Unet",
+        "Output": "Mean 0-2 Category TCs over 10 years",
+        "model": SongUNet.load_model(),
+    },
     {
         "Name": "Custom UNet",
         "Output": "Mean 0-2 Category TCs over 10 years",
