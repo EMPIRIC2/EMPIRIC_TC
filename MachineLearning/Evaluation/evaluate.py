a21691b004868913cfe29c8ea1d1d63169728161
Eli Winkelman <eliwinkelman2@gmail.com>
2024-06-12 15:20:25 +1200

    add nearest_neighbors to evaluation pipeline
    Former-commit-id: 2784074aab423ad0989f5b47a43e8c8d31a3769d

--- MachineLearning/Evaluation/evaluate.py
+++ MachineLearning/Evaluation/evaluate.py
@@ -2,13 +2,16 @@ import argparse
 from MachineLearning.Evaluation.evaluation_utils import process_predictions
 from MachineLearning.Evaluation.model_statistics import compute_ensemble_statistics
 from MachineLearning.Evaluation.metrics import compute_metrics
-from MachineLearning.Evaluation.model_info import models_info
 from MachineLearning.Evaluation.figures import make_figures, save_metrics_as_latex
 from MachineLearning.dataset import get_dataset
+from MachineLearning.NearestNeighbors.nearest_neighbors import NearestNeighborsRegressor
 import numpy as np
 import os
+from saved_models.saved_models import DDPMUNet02CatCyclones, DDPMUNetNoAttention02CatCyclones, UNetCustom02CatCyclones, SongUNet
 
-def evaluate(dataset, output_dir, predict_params):
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def evaluate(outputs_ds, output_dir):
     """
     Compute all evaluation metrics and then save the resulting figures to disk.
 
@@ -17,20 +20,20 @@ def evaluate(dataset, output_dir, predict_params):
     :param: output_save_folder: folder to save metrics latex and figure pictures
     """
 
-    outputs_ds = dataset.map(lambda x, y: y)
-    outputs = np.squeeze(np.concatenate(list(outputs_ds.as_numpy_iterator()), axis=0))
-
-    inputs = dataset.map(lambda x,y: x)
-
     metrics = []
+
+    batch_axis = 0
+    unbatched_outputs = np.concatenate(list(outputs_ds.as_numpy_iterator()), axis=batch_axis)
+    outputs = np.squeeze(unbatched_outputs)
     storm_statistics = compute_ensemble_statistics("STORM", outputs)
 
     for model_info in models_info:
+
         model = model_info["model"]
+        inputs = model_info["inputs"]
 
         predictions = model.predict(
-            inputs,
-            **predict_params
+            inputs
         )
         
         predictions = process_predictions(predictions)
@@ -48,7 +51,6 @@ def evaluate(dataset, output_dir, predict_params):
 
     save_metrics_as_latex(metrics, os.path.join(output_dir, "metrics.tex"))
 
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
@@ -61,11 +63,54 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    test_data = get_dataset(args.data_folder, data_version=4, dataset='test', batch_size=32)
+    test_data_ml = get_dataset(args.data_folder, data_version=4, dataset='test', batch_size=32)
+
+    outputs_ds = test_data_ml.map(lambda x, y: y)
+    ml_inputs = test_data_ml.map(lambda x, y: x)
+
+    test_data_nearest_neighbors = get_dataset(args.data_folder, data_version=5, dataset='test', batch_size=32)
+    nearest_neighbors_inputs = test_data_nearest_neighbors.map(lambda x, y: x)
+    nearest_neighbors_regressor = NearestNeighborsRegressor(args.data_folder)
+    nearest_neighbors_regressor.load(os.path.join(__location__, '../NearestNeighbors/nearest_neighbors.pkl'))
+    """
+    Object keeps track of the models we want to evaluate.
+    """
+    models_info = [
+        {
+            "Name": "DDIM Unet",
+            "Output": "Mean 0-2 Category TCs over 10 years",
+            "model": SongUNet.load_model(),
+            "inputs": ml_inputs
+        },
+        {
+            "Name": "Custom UNet",
+            "Output": "Mean 0-2 Category TCs over 10 years",
+            "model": UNetCustom02CatCyclones.load_model(),
+            "inputs": ml_inputs
+        },
+        {
+            "Name": "DDPM UNet",
+            "Output": "Mean 0-2 Category TCs over 10 years",
+            "model": DDPMUNet02CatCyclones.load_model(),
+            "inputs": ml_inputs
+        },
+        {
+            "Name": "DDPM UNet w/o attention",
+            "Output": "Mean 0-2 Category TCs over 10 years",
+            "model": DDPMUNetNoAttention02CatCyclones.load_model(),
+            "inputs": ml_inputs
+        },
+        {
+            "Name": "Nearest Neighbors Regressor",
+            "Output": "Mean 0-2 Category TCs over 10 years",
+            "model": nearest_neighbors_regressor,
+            "inputs": nearest_neighbors_inputs
+        }
+    ]
 
     predict_params = {
         "batch_size": 32,
         "verbose": 2
     }
 
-    evaluate(test_data, args.output_save_folder, predict_params)
+    evaluate(outputs_ds, args.output_save_folder)
--- MachineLearning/Evaluation/relative_change_metrics.py
+++ MachineLearning/Evaluation/relative_change_metrics.py
@@ -1,6 +1,7 @@
 from sklearn.metrics import mean_squared_error
 import itertools
 import numpy as np
+from MachineLearning.Evaluation.site_metrics import site_mean_squared_error
 
 def relative_change(a, b, eps=1e-5):
     """
@@ -21,7 +22,7 @@ def compute_all_relative_change_pairs(ground_outputs, model_outputs, max_pairs =
     pairs = itertools.combinations(range(len(ground_outputs)), 2)
 
     error_maps = []
-    mean_squared_errors = []
+    site_mean_squared_errors = []
     for i, pair in enumerate(pairs):
 
         if i > max_pairs:
@@ -39,14 +40,14 @@ def compute_all_relative_change_pairs(ground_outputs, model_outputs, max_pairs =
 
         error_map = ground_change - model_change
         mse = mean_squared_error(ground_change, model_change)
+        site_mse = site_mean_squared_error(ground_change, model_change)
 
         error_maps.append(error_map)
-        mean_squared_errors.append(mse)
+        site_mean_squared_errors.append(site_mse)
 
-    total_mse = np.mean(mean_squared_errors)
-
+    total_site_mse = np.mean(site_mean_squared_errors)
     n_examples = min(10, len(ground_outputs))
-    largest_error_indices = np.argpartition(mean_squared_errors, -n_examples)[-n_examples:]
+    largest_error_indices = np.argpartition(site_mean_squared_errors, -n_examples)[-n_examples:]
     top_error_maps = np.array(error_maps)[largest_error_indices]
 
-    return top_error_maps, total_mse
+    return top_error_maps, total_site_mse
