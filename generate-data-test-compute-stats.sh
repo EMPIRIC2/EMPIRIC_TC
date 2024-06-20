fb7432f15f323975abed095eaca05fa5bbf3ca21
Eli Winkelman <ewin313@wbn003.cm.cluster>
2024-06-20 12:55:05 +1200

    update run script
    Former-commit-id: 189885ed5864266e7940a0f021921b5c793f085e

--- generate-data-test-compute-stats.sh
+++ generate-data-test-compute-stats.sh
@@ -1,7 +1,7 @@
 python slurm-launch.py \
   --exp-name test_data_generation \
-  --command "python ./TrainingDataGeneration/GenerateTrainingData.py 25 1 0 0 ./Data/run6 --include_grids --compute_stats --constant_historical_inputs" \
+  --command "python ./TrainingDataGeneration/GenerateTrainingData.py 100 1 0 0 ./Data/run6 --include_grids --compute_stats --constant_historical_inputs" \
   --load-env "source load-environment.sh" \
-  --mem "600MB" \
-  --time "00:10:00" \
+  --mem "1GB" \
+  --time "00:15:00" \
   --debug True
