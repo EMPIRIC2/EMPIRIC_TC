1f80279e98ddf5eb9b97e90204b1bc96794f8b13
Eli Winkelman <eliwinkelman2@gmail.com>
2024-06-24 11:53:50 +1200

    update data generation scripts
    Former-commit-id: 222bb847ac502bdb3e3e703f9cd919bb645350a2

--- scripts/generate-data.sh
+++ scripts/generate-data.sh
@@ -1,7 +1,7 @@
 
 python slurm-launch.py \
   --exp-name data_generation \
-  --command "python ../TrainingDataGeneration/GenerateTrainingData.py 1000 25 5 5 /nesi/project/uoa03669/ewin313/storm_data/v5 --include_grids" \
+  --command "python ../TrainingDataGeneration/GenerateTrainingData.py 1000 25 5 5 /nesi/project/uoa03669/ewin313/storm_data/v5 10 100" \
   --load-env "source load-environment.sh" \
   --mem "2GB" \
   --time "018:00:00" \
--- scripts/generate-grid-data.sh
+++ scripts/generate-grid-data.sh
@@ -1,6 +1,6 @@
 python slurm-launch.py \
   --exp-name grid_data_generation \
-  --command "python ./Training\ Data\ Generation/GenerateTrainingData.py 1000 1 0 0 /nesi/project/uoa03669/ewin313/storm_data/v4" \
+  --command "python ../TrainingDataGeneration/GenerateTrainingData.py 1000 1 0 0 /nesi/project/uoa03669/ewin313/storm_data/v4 10 100" \
   --load-env "source load-environment.sh" \
   --mem "1GB" \
   --time "06:00:00" \
