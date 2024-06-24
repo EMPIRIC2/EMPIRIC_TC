088dfafed08135b681c43a098bdc5ea9801fd24e
Eli Winkelman <eliwinkelman2@gmail.com>
2024-06-24 13:52:47 +1200

    move scripts to script directory
    Former-commit-id: bba0228b1345f60d60127fad3476bfbbbb852de0

--- generate-data-compute-stats.sh
+++
@@ -1,6 +0,0 @@
-python slurm-launch.py \
-  --exp-name data_generation_stats \
-  --command "python ./TrainingDataGeneration/GenerateTrainingData.py 10000 1 0 0 ./Data/stats_10000 --include_grids --compute_stats" \
-  --load-env "source load-environment.sh" \
-  --mem "2GB" \
-  --time "18:00:00" \
